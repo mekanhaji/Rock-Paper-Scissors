e43cbb41c3d005f931d4a8a0c23dfc631438aee5
kanhaji <myself.kanhaji@gmail.com>
2023-12-03 11:08:39 +0530

    TEST production

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,18 +1,20 @@
+const cachesFiles = [
+  "./",
+  "./index.html",
+  "./src/style.css",
+  "./src/script.js",
+  "./assets/favicon.png",
+  "./assets/icons/icon-72x72.png",
+  "./assets/icons/icon-96x96.png",
+  "./assets/icons/icon-128x128.png",
+  "./assets/icons/icon-144x144.png",
+  "./assets/icons/icon-152x152.png",
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("my-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/src/style.css",
-        "/src/script.js",
-        "assets/favicon.png",
-        "/assets/icons/icon-72x72.png",
-        "/assets/icons/icon-96x96.png",
-        "/assets/icons/icon-128x128.png",
-        "/assets/icons/icon-144x144.png",
-        "/assets/icons/icon-152x152.png",
-      ]);
+      return cache.addAll(cachesFiles);
     })
   );
 });
